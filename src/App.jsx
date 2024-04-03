2f7b5fb1d7ffeeaafc7d02ec5e3ed139cd4c5025
Vishwassukhija <vishwassukhija@gmail.com>
2024-04-03 19:00:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="bg-green">Hello</div>;
+};
+
+export default App;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaHeart } from "react-icons/fa";
+const Cards = ({ item }) => {
+  const [isHeartFillted, setIsHeartFillted] = useState(false);
+  const handleHeartClick = () => {
+    setIsHeartFillted(!isHeartFillted);
+  };
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div
+        className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${
+          isHeartFillted ? "text-rose-500" : "text-white"
+        }`}
+        onClick={handleHeartClick}
+      >
+        <FaHeart className=" h-5 w-5 cursor-pointer" />
+      </div>
+      <Link to={`/menu/${item.id}`}>
+        <figure>
+          <img src={item.image} alt="" />
+        </figure>
+      </Link>
+      <div className="card-body">
+        <h2 className="card-title">{item.name}</h2>
+        <p>{item.recipe}</p>
+        <div className="card-actions justify-end">
+          <h5 className="font-semibold mr-40 mt-4">
+            <span className="text-sm text-red ">$</span>
+            {item.price}
+          </h5>
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
